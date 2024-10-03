beb9e6f3b459a58a6adf18b550a1a4fffc8a9973
Atika Hamid <hafizaatikahamid965@gmail.com>
2024-10-03 14:46:55 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import PageOne from './Components/PageOne'
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import PageTwo from './Components/PageTwo';
+import PageThree from './Components/PageThree';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<PageOne/>}/>
+        <Route path="/about" element={<PageTwo/>}/>
+        <Route path="/contact" element={<PageThree/>}/>
+
+      </Routes>
     </div>
-  );
+  )
 }
 
 export default App;
